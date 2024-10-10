31d7bfd8f404b74f92697ef5d54a1a805ed9f52d
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-10-10 19:45:49 -0300

    feat: reading stop words

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import argparse
 from message_parser import WhatsappChatParser
+import os
+import unicodedata
 
 
 def main(args):
     with open(args.filename, 'r') as rf:
         messages, people = WhatsappChatParser.parse(rf, args.dateformat)
+
+
+def normalize_str(s: str):
+    return ''.join(c for c in unicodedata.normalize('NFD', s.strip())
+        if unicodedata.category(c) != 'Mn').lower()
+
+
+def get_stop_words(directory: str):
+    stop_words = set()
+    for file in os.listdir(directory):
+        with open(os.path.join(directory, file), 'r') as rf:
+            words = rf.read().splitlines()
+        stop_words.update([normalize_str(w) for w in words])
+    return stop_words
 
 
 def parse_args():
