72db319fe346b74bcf46975339d97c1fda575d72
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-10-10 21:04:50 -0300

    feat: most common words

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import argparse
-from message_parser import WhatsappChatParser
+from message_parser import WhatsappChatParser, WhatsappMessage
 import os
 import unicodedata
 
 
 def main(args):
+    stop_words = read_wordlist_dir('stop_words')
+    swear_words = read_wordlist_dir('swear_words')
+
     with open(args.filename, 'r') as rf:
         messages, people = WhatsappChatParser.parse(rf, args.dateformat)
+    
+
+def most_common_words(messages: list[WhatsappMessage], stop_words: set[str]):
+    word_count = {}
+    for m in messages:
+        for word in m.content.split():
+            if word in stop_words:
+                continue
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+    return {k: v for k, v in sorted(word_count, key=lambda item: item[1], reverse=True)}
 
 
 def normalize_str(s: str):
@@ -14,13 +30,17 @@ def normalize_str(s: str):
         if unicodedata.category(c) != 'Mn').lower()
 
 
-def get_stop_words(directory: str):
-    stop_words = set()
+def read_wordlist(filename: str):
+    with open(filename, 'r') as rf:
+        words = rf.read().splitlines()
+    return set([normalize_str(w) for w in words])
+
+
+def read_wordlist_dir(directory: str):
+    words = set()
     for file in os.listdir(directory):
-        with open(os.path.join(directory, file), 'r') as rf:
-            words = rf.read().splitlines()
-        stop_words.update([normalize_str(w) for w in words])
-    return stop_words
+        words.update(read_wordlist(os.path.join(directory, file)))
+    return words
 
 
 def parse_args():
